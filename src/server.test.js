70d4ddbe674dd4e88b445067c4d833d4bf9ddb13
Opeyemi <opeyemiatanda23@yahoo.com>
2025-07-01 21:34:22 -0700

    updating bash error

--- src/server.test.js
+++ src/server.test.js
@@ -12,7 +12,7 @@ describe('root', () => {
       .get('/')
       .expect(200)
       .then((res) => {
-        expect(res.text).toContain('Welcome to Express');
+        expect(res.text).toContain('Developments of Cloud DevOps');
       }));
 
   it('request api, returns json', () => request(app)
